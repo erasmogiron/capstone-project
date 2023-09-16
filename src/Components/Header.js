5f8b8b496775c089305eb6932faac15771ce0178
erasmogiron <133612440+erasmogiron@users.noreply.github.com>
2023-09-15 20:05:24 -0700

    Add link to logo on Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import logo from '../images/logo.svg';
 import Nav from './Nav';
+import { Link } from "react-router-dom";
 
 export default function Header() {
     return(
         <header className="container">
             <div className="row flex">
-                <img src={logo} alt="Little Lemon" />
+                <Link to="/"><img src={logo} alt="Little Lemon" /></Link>
                 <Nav />
             </div>
         </header>
