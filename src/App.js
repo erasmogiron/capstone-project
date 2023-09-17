3c0e17805bae835a0c2357870303a906fde81814
erasmogiron <133612440+erasmogiron@users.noreply.github.com>
2023-09-16 17:22:16 -0700

    Fix BookingPage route in App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import HomePage from './HomePage';
 import OurStoryPage from './OurStoryPage';
 import MenuPage from './MenuPage';
 import OrderOnlinePage from './OrderOnlinePage';
-import ReservationsPage from './ReservationsPage';
+import BookingPage from './BookingPage';
 import LoginPage from './LoginPage';
 import {Routes, Route} from "react-router-dom";
 
@@ -14,7 +14,7 @@ function App() {
             <Route path="/" element={<HomePage />}></Route>
             <Route path="/about" element={<OurStoryPage />}></Route>
             <Route path="/menu" element={<MenuPage />}></Route>
-            <Route path="/reservations" element={<ReservationsPage />}></Route>
+            <Route path="/reservations" element={<BookingPage />}></Route>
             <Route path="/order-online" element={<OrderOnlinePage />}></Route>
             <Route path="/login" element={<LoginPage />}></Route>
         </Routes>
